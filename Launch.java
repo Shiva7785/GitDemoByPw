07f9116e4487211e6c105ebf51d298950a31cec2
Shiva7785 <shivanandreddy146@gmail.com>
2025-03-23 15:24:34 +0530

    Modified Afte Commit...

--- Launch.java
+++ Launch.java
@@ -1,9 +1,17 @@
 class launch
 {
+
          public static void main(String[] args)
          {
 
                    System.out.println("Exploring Git and GitHub... " );
 
+         }
+         public static voif fun(){
+            int a = 10;
+            int b = 20;
+            int sum = a+b;
+
+            System.out.println(sum);
          }
 }
